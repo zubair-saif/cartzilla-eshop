2af72689f84688f56b3b370b960173d2fdeddc1c
zubair-saif <zubairsaif700@gmail.com>
2021-08-06 11:04:20 +0500

    category page design completed

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
+import { DataService } from '../service/data.service';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
+  public productList: any = [];
+  public isLoading: boolean = false;
+  p: number = 1;
+  constructor(private data: DataService) { }
 
   ngOnInit(): void {
+    this.isLoading = true;
+    this.data.getAllProduct().subscribe((res: Product) => {
+      this.productList = res;
+      this.isLoading = false;
+    })
   }
-
+  addToCart(productId: Number) {
+    let addToCart = this.productList.filter((item: Product) => {
+      return item.id === productId;
+    });
+    this.data.addProductToCart(addToCart[0]);
+  }
 }
