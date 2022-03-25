a6fdbf2cc44c55fed665be94abccc4090b80cb8d
zubair-saif <zubairsaif700@gmail.com>
2022-03-25 20:17:29 +0500

    dynamic year change in footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  date = new Date();
 
   constructor() { }
-
   ngOnInit(): void {
   }
 
