80110fc3fb94373b4538f65f5754eec1f37aac14
zubair-saif <zubairsaif700@gmail.com>
2022-03-27 16:45:41 +0500

    new sticky navigation added based scroll

--- src/app/shared/navbars/navbars.component.ts
+++ src/app/shared/navbars/navbars.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 import { DataService } from 'src/app/service/data.service';
 
 @Component({
   selector: 'app-navbars',
   templateUrl: './navbars.component.html',
-  styleUrls: ['./navbars.component.scss']
+  styleUrls: ['./navbars.component.scss'],
+  animations: [
+    trigger('fade',
+      [
+        state('void', style({ opacity: 0 })),
+        transition(':enter', [animate(300)]),
+        transition(':leave', [animate(500)]),
+      ]
+    )]
 })
 export class NavbarsComponent implements OnInit {
   public cartProductCount: number = 0;
-  constructor(private data: DataService) { }
+
+  constructor(private data: DataService, @Inject(DOCUMENT) public document: Document) { }
 
   ngOnInit(): void {
     this.data.getProductsCount().subscribe(res => {
@@ -16,4 +27,13 @@ export class NavbarsComponent implements OnInit {
     })
   }
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e: Event) {
+    if (window.pageYOffset > 400) {
+      this.document.getElementById('navbar')?.classList.add('sticky');
+    } else {
+      this.document.getElementById('navbar')?.classList.remove('sticky');
+
+    }
+  }
 }
